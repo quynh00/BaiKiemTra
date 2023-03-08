3e201c31ed57ab3d8394c6eaae8b45e3fe49c0d0
Students <nghiemthuyquynh48@gmail.com>
2023-03-08 16:26:32 +0700

    update

--- src/Cau_3/Main.java
+++ src/Cau_3/Main.java
@@ -6,8 +6,10 @@ public class Main {
     public static void main(String[] args) {
         VanBan chuoi1 = new VanBan("nghiem thuy quynh");
         System.out.println(chuoi1.vietHoa());
+        System.out.println("Số từ của chuỗi là: "+ chuoi1.demTu());
+        chuoi1.vietHoaChuDau();
 
-        VanBan chuoi2 = new VanBan("Nghiem thuy QUYNH");
+        VanBan chuoi2 = new VanBan("\nNghiem thuy QUYNH");
         System.out.println(chuoi2.vietThuong());
 
     }
--- src/Cau_3/VanBan.java
+++ src/Cau_3/VanBan.java
@@ -18,6 +18,10 @@ public class VanBan {
     public void setSt(String st) {
         this.st = st;
     }
+    //Đếm số từ của văn bản
+    public int demTu(){
+        return st.length();
+    }
     //Chuyển toàn bộ văn bản thành viết hoa
     public String vietHoa(){
         return st.toUpperCase();
@@ -26,5 +30,15 @@ public class VanBan {
     public String vietThuong(){
         return st.toLowerCase();
     }
+    public void vietHoaChuDau(){
+        System.out.println("Chuỗi viết hoa chữ cái đầu: " +
+                st.substring(0, 1).toUpperCase() + st.substring(1));
+    }
 
+    @Override
+    public String toString() {
+        return "VanBan{" +
+                "st='" + st + '\'' +
+                '}';
+    }
 }
